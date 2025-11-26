c339a2c0c4e7f409326372331e078ac5920b7911
shangjung <david103132881@gmail.com>
2025-11-26 10:33:25 +0800

    add: update text-to-speech scripts with new content and output file adjustments

--- tts/google/test.py
+++ tts/google/test.py
@@ -1,16 +1,33 @@
 from google.cloud import texttospeech
 import os
+from pathlib import Path
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "./tts/tts_credential.json"
+script_dir = Path(__file__).parent
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = str(script_dir / "tts_credential.json")
 client = texttospeech.TextToSpeechClient()
 
-text = "hello, I'm fine, thank you. And you?"
+# text = "hello, I'm fine, thank you. And you?"
+text = '''
+本文將逐步說明如何使用雙向串流合成音訊。
+
+雙向串流可讓您同時傳送文字輸入內容及接收音訊資料。也就是說，在傳送完整輸入文字前即可開始合成語音，這樣就能縮短延遲時間，並進行即時互動。語音助理和互動式遊戲會使用雙向串流，打造回應迅速的動態應用程式。
+
+如要進一步瞭解 Text-to-Speech 的基本概念，請參閱「Text-to-Speech 基本概念」。
+'''
 
 synthesis_input = texttospeech.SynthesisInput(text=text)
 
+
+# https://docs.cloud.google.com/text-to-speech/docs/list-voices-and-types?hl=zh-tw
+
+# voice = texttospeech.VoiceSelectionParams(
+#     language_code="en-US",
+#     name="en-US-Chirp-HD-O"
+# )
+
 voice = texttospeech.VoiceSelectionParams(
-    language_code="en-US",
-    name="en-US-Chirp-HD-O"
+    language_code="cmn-TW",
+    name="cmn-TW-Wavenet-C"
 )
 
 audio_config = texttospeech.AudioConfig(
@@ -26,6 +43,6 @@ response = client.synthesize_speech(
     audio_config=audio_config
 )
 
-with open("output.mp3", "wb") as out:
+with open(f"{script_dir}/output.mp3", "wb") as out:
     out.write(response.audio_content)
     print('Audio content written to file "output.mp3"')
--- tts/yating/test.py
+++ tts/yating/test.py
@@ -8,7 +8,13 @@ load_dotenv()
 URL = "https://tts.api.yating.tw/v2/speeches/short"
 KEY = os.getenv("YATING_API_KEY")
 
-TEXT = "歡迎收聽雅婷文字轉語音"
+TEXT = '''
+本文將逐步說明如何使用雙向串流合成音訊。
+
+雙向串流可讓您同時傳送文字輸入內容及接收音訊資料。也就是說，在傳送完整輸入文字前即可開始合成語音，這樣就能縮短延遲時間，並進行即時互動。語音助理和互動式遊戲會使用雙向串流，打造回應迅速的動態應用程式。
+
+如要進一步瞭解 Text-to-Speech 的基本概念，請參閱「Text-to-Speech 基本概念」。
+'''
 TEXT_TYPE = ttsClient.TYPE_TEXT
 MODEL = ttsClient.MODEL_ZHEN_FEMALE_1
 SPEED = 1.0
@@ -19,7 +25,7 @@ SAMPLE_RATE = ttsClient.SAMPLE_RATE_16K
 
 script_dir = Path(__file__).parent
 output_dir = script_dir
-FILE_NAME = Path(output_dir) / "yating_output.mp3"
+FILE_NAME = Path(output_dir) / "yating_output"
 
 try:
     client = ttsClient(URL, KEY)
